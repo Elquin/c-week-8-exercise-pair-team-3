0bf61ced613ee4ddc44794f43aa70eb28afdc87b
Robert Holland <robertrhollandiii@gmail.com>
2019-11-08 14:11:32 -0500

    Exercise

---
+++ student-exercise/Validation.Web/Controllers/UsersController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Validation.Web.Models;
+
+
+namespace Validation.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: User
+        //public IActionResult Index()   ????
+        //{
+        //    return View("Index");
+        //}
+
+        // GET: User/Register
+        // Return the empty registration view
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: User/Register
+        // Validate the model and redirect to confirmation (if successful) or return the 
+        // registration view (if validation fails)    
+        [HttpPost]
+        public IActionResult Register(RegistrationViewModel register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(register);
+            }
+
+            return RedirectToAction("ConfirmRegister");
+        }
+
+        // GET: User/Login
+        // Return the empty login view
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: User/Login  
+        // Validate the model and redirect to login (if successful) or return the 
+        // login view (if validation fails)
+        [HttpPost]
+        public IActionResult Login(LoginViewModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            return RedirectToAction("ConfirmLogin");
+
+        }
+
+        // GET: User/Confirmation
+        // Return the confirmation view
+        [HttpGet]
+        public IActionResult ConfirmLogin()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ student-exercise/Validation.Web/Models/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Validation.Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Please supply an Email")]
+        [EmailAddress(ErrorMessage ="Please supply a valid Email. (e.g., johnDoe@aol.com")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Please supply a password")]
+        public string Password { get; set; }
+
+        public LoginViewModel() { }
+
+        public LoginViewModel(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+    }
+}
